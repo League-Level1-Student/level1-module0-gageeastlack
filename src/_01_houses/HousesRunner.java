dc229d0dc671138a48bf3248e1b5a2726345f997
root <root@1b7a04ae5d83>
2021-03-06 20:56:14 +0000

    Finished some, still need to finish the rest.

--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -1,12 +1,26 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class HousesRunner {
 
 	public static void main(String[] args) {
 		//Here we call the run() method from the Houses class
 		Houses houseBuilder = new Houses();
 		houseBuilder.run();
-
+		Robot rob = new Robot();
+		rob.penDown();
+		rob.moveTo(50, 600);
+		rob.setSpeed(10);
+		rob.move(150);
+		rob.turn(45);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(45);
+		rob.move(150);
+		rob.turn(270);
+		rob.move(100);
 
 	}
 
