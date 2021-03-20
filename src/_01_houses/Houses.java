a43d87d5bdebab22e3a2f40d365f85837bb17087
root <root@bc590042dff5>
2021-03-20 19:55:25 +0000

    Almost completed Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,30 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot rob = new Robot();
 	public void run() {
-		
+		rob.moveTo(15, 600);
+		drawHouse();
+		drawHouse();
+		drawHouse();
+		drawHouse();
+		drawHouse();
+	} 
+		void drawHouse() {
+			rob.penDown();
+			rob.setSpeed(10);
+			rob.move(150);
+			rob.turn(45);
+			rob.move(100);
+			rob.turn(90);
+			rob.move(100);
+			rob.turn(45);
+			rob.move(150);
+			rob.turn(270);
+			rob.move(40);
+			rob.turn(270);
+		}
 	}
-}
+
